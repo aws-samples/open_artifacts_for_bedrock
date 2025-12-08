a59524aff3435abf1b4daf92b697070b6a4b0b4a
River Xie <xiehust@163.com>
2025-12-08 09:07:16 +0000

    update model id

--- src/constant.py
+++ src/constant.py
@@ -5,6 +5,7 @@ CLAUDE_35_HAIKU_MODEL_ID = 'anthropic.claude-3-5-haiku-20241022-v1:0'
 CLAUDE_35_SONNET_MODEL_ID = 'anthropic.claude-3-5-sonnet-20241022-v2:0'
 CLAUDE_45_SONNET_MODEL_ID = 'anthropic.claude-sonnet-4-5-20250929-v1:0'
 CLAUDE_45_HAIKU_MODEL_ID = 'anthropic.claude-haiku-4-5-20251001-v1:0'
+CLAUDE_45_OPUS_MODEL_ID = 'anthropic.claude-opus-4-5-20251101-v1:0'
 NOVA_RPO_MODEL_ID = 'us.amazon.nova-pro-v1:0'
 NOVA_LITE_MODEL_ID = 'us.amazon.nova-lite-v1:0'
 
@@ -14,6 +15,7 @@ def is_claude_thinking(model_id):
             or CLAUDE_4_OPUS_MODEL_ID in model_id
             or CLAUDE_45_SONNET_MODEL_ID in model_id 
             or CLAUDE_45_HAIKU_MODEL_ID in model_id
+            or CLAUDE_45_OPUS_MODEL_ID in model_id
             )
     
 def is_interleaved_claude_thinking(model_id):
@@ -21,6 +23,7 @@ def is_interleaved_claude_thinking(model_id):
             or CLAUDE_4_OPUS_MODEL_ID in model_id
             or CLAUDE_45_SONNET_MODEL_ID in model_id
             or CLAUDE_45_HAIKU_MODEL_ID in model_id
+            or CLAUDE_45_OPUS_MODEL_ID in model_id
             )
     
 def is_prompt_cache(model_id):
@@ -29,4 +32,5 @@ def is_prompt_cache(model_id):
         or CLAUDE_4_OPUS_MODEL_ID in model_id
         or CLAUDE_45_SONNET_MODEL_ID in model_id 
         or CLAUDE_45_HAIKU_MODEL_ID in model_id
+        or CLAUDE_45_OPUS_MODEL_ID in model_id
         )
